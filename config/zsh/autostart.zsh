bf2dfd3de8bac2496290d06250cdda844426727e
wuqin <1392632107@qq.com>
2024-11-04 12:45:57 +0800

    增加没有XDG_RUNTIME_DIR变量时的判定

--- config/zsh/autostart.zsh
+++ config/zsh/autostart.zsh
@@ -1,13 +1,14 @@
 # ssh-agent
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
-fi
-if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+if [ -n $XDG_RUNTIME_DIR ]; then
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+        ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+    fi
+    if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
+        source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+    fi
 fi
 
 # conda
 conda_path=/opt/$(ls /opt | grep -i conda)/etc/profile.d/conda.sh
 [ -e "$conda_path" ] && source $conda_path
-
 unset conda_path
